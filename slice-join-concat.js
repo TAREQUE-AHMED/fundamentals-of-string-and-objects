db7227c264988d6052efa9a0f65bd6a71bf6d4a3
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2024-09-21 21:11:11 +0600

    slice-join-concat

---
+++ slice-join-concat.js
@@ -0,0 +1,22 @@
+const address = `Sirajganj`;
+console.log(address);
+console.log(address.slice(3, 7));
+
+
+
+
+const name = 'I love to work in cyber security sector';
+console.log(name.split(' '));
+
+const firstName = `Tareque`;
+const lastName = `Ahmed`;
+const fullName = firstName+ ' ' + lastName;
+console.log(fullName);
+
+const first = `Ahmed`;
+const last = `Tareque`;
+const fullName2 = first.concat('').concat(last);
+console.log(fullName2);
+
+console.log(last.includes('a'));
+
